cc0813ae6603822201fb939695b2dcb4fb508f3c
Trenton <trenton.kuzma@tru-cs.com>
2016-02-05 15:02:28 -0700

    Final

---
+++ Receive1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("----------Happy Receiver----------");
+            Console.WriteLine("Press [Enter] to exit.");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "Emotional_Messages",
+                    type: "direct");
+                var queueName = channel.QueueDeclare().QueueName;
+
+
+                channel.QueueBind(queue: queueName,
+                    exchange: "Emotional_Messages",
+                    routingKey: "Happy");
+
+                Console.WriteLine("Waiting for messages...");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" Received {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                    noAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var message = "";
+            var closeApp = false;
+            Console.WriteLine("----------Sender----------");
+            Console.WriteLine("Write a message to send... (include the word \"happy\" or \"sad\")");
+            Console.WriteLine("...or [Enter] to exit");
+
+            do
+            {
+                if (!string.IsNullOrEmpty(message))
+                {
+
+                    var factory = new ConnectionFactory() {HostName = "localhost"};
+                    using (var connection = factory.CreateConnection())
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.ExchangeDeclare(exchange: "Emotional_Messages",
+                            type: "direct");
+
+                        var isHappy = message.ToLower().IndexOf("happy", StringComparison.Ordinal) > -1;
+
+                        var body = Encoding.UTF8.GetBytes(message);
+                        channel.BasicPublish(exchange: "Emotional_Messages",
+                            routingKey: isHappy ? "Happy" : "Sad",
+                            basicProperties: null,
+                            body: body);
+                        Console.WriteLine(" Sent... {0}", message);
+                    }
+                }
+
+                message = Console.ReadLine();
+                if (string.IsNullOrEmpty(message))
+                {
+                    closeApp = true;
+                }
+
+            } while (!closeApp);
+
+            Environment.Exit(0);
+        }
+    }
+}
